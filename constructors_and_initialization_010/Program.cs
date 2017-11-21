a27ecf66a27c218dbcfdd76c59a54615bb1ce365
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-21 23:14:33 +1100

    First commit

---
+++ constructors_and_initialization_010/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace constructors_and_initialization_010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

5c3fe3fb52558010b59ab8484cbba5498127085c
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-21 23:19:33 +1100

    Added employee Mary

--- constructors_and_initialization_010/Program.cs
+++ constructors_and_initialization_010/Program.cs
@@ -6,7 +6,20 @@ namespace constructors_and_initialization_010
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee Dave = new Employee();
+            {
+                Age = 35;
+                Name = "David Smith";
+                Salary = 50000.00;
+                StartingDate = new DateTime(2012, 7, 10);
+                PhoneNumber = "617-555-1234";
+            }
+            Console.WriteLine("Dave's age is {0} he started on {1} and makes {2}",
+                              Dave.Age, Dave.startingDate, Dave.Salary);
+
+            Employee Mary = new Employee(25, "Mary Jones", 60000, new DateTime(2010, 2, 28));
+            Console.WriteLine("\n\nMary's age is {0} she started on {1} and makes {2}",
+                              Mary.Age, Mary.StartingDate, Mary.Salary);
         }
     }
 }
